460c313a45d0ae0743f62b830afebe9ec629e960
Daine Mawer <hello@dainemawer.com>
2022-11-26 14:43:41 +0200

    feat: add card component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -3,22 +3,32 @@ import styles from './Card.module.css'
 import { formatDate } from '@util/date'
 import { classNames } from '@util/classes'
 
-export const Card = ({ date, title, description, slug, lead }) => {
+interface CardProps {
+	date: string
+	title: string
+	description: string
+	slug: string
+	lead?: boolean
+}
+
+export const Card = ({ date, title, description, slug, lead }: CardProps) => {
 
 	const classes = classNames(
 		styles.card,
 		lead && styles.lead
-	)
+	);
 
 	return (
-		<article className={classes}>
-			<div>
-				<time dateTime={date} className={styles.date}>
-					<span className={styles.time}>{formatDate(date)}</span>
-				</time>
-				<h2 className={styles.title}><Link className={styles.anchor} href={slug}>{title}</Link></h2>
-				<p className={styles.description}>{description}</p>
-			</div>
+		<article aria-label={title} id={`post-${slug}`} className={classes}>
+			<time dateTime={date} className={styles.date}>
+				<span className={styles.time}>{formatDate(date)}</span>
+			</time>
+			<h3 className={styles.title}>
+				<Link className={styles.anchor} href={`/articles/${slug}`}>{title}</Link>
+			</h3>
+			<p className={styles.description}>
+				{description}
+			</p>
 		</article>
 	)
 }
