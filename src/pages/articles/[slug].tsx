b8f0dcd2d480f16fbe45905f8e4afdcbfce0b8cd
Daine Mawer <hello@dainemawer.com>
2022-11-26 14:44:57 +0200

    Updated styling and logic for index

--- src/pages/articles/[slug].tsx
+++ src/pages/articles/[slug].tsx
@@ -3,6 +3,11 @@ import { GetStaticPaths, GetStaticProps } from 'next'
 import convertToHTML from '@util/remark'
 import { useRouter } from 'next/router'
 import ErrorPage from 'next/error'
+import Layout from '@components/Layout'
+import { formatDate } from '@util/date'
+import Link from 'next/link'
+
+import styles from './Article.module.css'
 
 interface Post {
 	post: {
@@ -36,17 +41,28 @@ export default function Post({ post }: Post) {
 	}
 
 	return (
-		<article>
-			<h1>{post.title}</h1>
-			<div dangerouslySetInnerHTML={{ __html: post.content }} />
-			<ul>
-				<li>Slug: {post.slug}</li>
-				<li>Title: {post.title}</li>
-				<li>Published At: {post.publishedAt}</li>
-				<li>Cover Image: {post.coverImage}</li>
-				<li>Author: {post.author.name}</li>
-			</ul>
-		</article>
+		<Layout>
+			<article className={styles.article}>
+				<ul className={styles.breadcrumbs}>
+					<li><Link href="/">Home</Link></li>
+					<li><Link href="/articles">Articles</Link></li>
+					<li>{post.title}</li>
+				</ul>
+				<h1 className={styles.title}>{post.title}</h1>
+				<ul className={styles.list}>
+					<li className={styles.item}>{formatDate(post.publishedAt)}</li>
+					<li className={styles.item}>
+						<ul className={styles.social}>
+							<li>Facebook</li>
+							<li>Twitter</li>
+							<li>LinkedIn</li>
+						</ul>
+					</li>
+				</ul>
+				<div className={styles.content} dangerouslySetInnerHTML={{ __html: post.content }} />
+			</article>
+
+		</Layout>
 	)
 
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,10 +6,8 @@
  * @returns {JSX.Element}
 */
 
-import Link from 'next/link'
 import { Card } from '@components/Card'
 import { Section } from '@components/Section'
-import Image from 'next/image'
 import Layout from '@components/Layout'
 import { NextSeo } from 'next-seo';
 import { getDocuments } from 'outstatic/server'
@@ -26,7 +24,7 @@ interface PostsProps {
 }
 
 export default function Home({ articles }: PostsProps): JSX.Element {
-	const { coverImage, title, description, slug, publishedAt } = articles[0];
+	const { title, description, slug, publishedAt } = articles[0];
 	const recent = articles.slice(1, 4);
 
 	return (
@@ -49,8 +47,6 @@ export default function Home({ articles }: PostsProps): JSX.Element {
 export const getServerSideProps = async () => {
 	const params = ['title', 'publishedAt', 'description', 'coverImage', 'slug']
 	const articles = getDocuments('articles', params)
-
-	console.log(articles);
 
 	return {
 		props: { articles }
